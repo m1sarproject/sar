ae5300ac4a14d385b75ff557aaec5345c71732e3
DIA <dia.idinarene@gmail.com>
2017-12-27 23:51:05 +0100

    Courtier version 1

--- src/com/m1sar/Courtier.java
+++ src/com/m1sar/Courtier.java
@@ -1,5 +1,25 @@
 package com.m1sar;
+/**@author Ouerdia
+ * 
+ */
+import java.util.ArrayList;
 
-public class Courtier {
+public class Courtier extends Thread{
+	
+private String name;
+private ArrayList<Client>customers;
+/**
+ * nbCustomer number of customers which are connected to this Broker
+ */
+private int nbCustomer;
+private double tauxCommission;
+private double accountBalance;
+
+/**
+ * Send to all his cutomers the updated price of the auction 
+ */
+public void sendAllPrices() {
+	
+}
 
 }
