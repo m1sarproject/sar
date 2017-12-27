b68fb96dadee0b9d4ad480adc3065b419cd38ca7
Nick3523 <lyes.meghara@gmail.com>
2017-12-27 13:48:53 +0100

    Le projet java a été crée via eclipse, à vous de l'importer, ajout de la première classe Bourse et de ses méthodes

---
+++ src/com/m1sar/Bourse.java
@@ -0,0 +1,151 @@
+package com.m1sar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bourse {
+
+	
+	private ArrayList<Entreprise> entreprises = new ArrayList<Entreprise> ();
+	private ArrayList<Courtier> courtiers = new ArrayList<Courtier> ();
+	
+	public Bourse() {
+		
+		
+	}
+	
+    /**@author Lyes
+     * At the end of the day, update the price of every business action in each company
+     * Follow this rule : new price = old prince + delta
+     * where delta = (number of purchase orders -  number of sale orders ) / number of buisness actions
+     */
+	
+	void updatePrice() {
+		
+		
+	}
+
+    /**@author Lyes
+     * Appends the specified Broker to the end of the list (If he is not alredy in the list).
+     * @param c : the element to be appended to this list
+     * IllegalArgumentException if the Broker does exist
+     * @return <tt>true</tt> 
+     */
+	boolean addBroker(Courtier c) {
+		
+		if ( courtiers.contains(c) ) throw new IllegalArgumentException("The Broker is already in the list.");
+
+		courtiers.add(c);
+		return true;
+
+	}
+	
+	
+    /**@author Lyes
+     * Appends the collection of Brokers to the end of the list, using addBroker(C).
+     * @param L : the collection of Brokers to be appended to this list
+     * @return <tt>true</tt> 
+     */
+	boolean addAllBrokers(List<Courtier> L) {
+		
+		for( Courtier c : L ) {
+			
+			addBroker(c);
+		}
+		
+		return true;
+
+		
+	}
+	
+	
+    /**@author Lyes
+     * Removes the specified Broker from the list (If he already is in the list).
+     * @param c : the element to be removed
+     * @exception : IllegalArgumentException if the Broker does not exist
+     * @return <tt>true</tt> 
+     */
+	boolean removeBroker(Courtier c) {
+		
+		if ( ! courtiers.contains(c) ) throw new IllegalArgumentException("The Broker does not exist.");
+
+		courtiers.remove(c);
+		
+		return true;
+	}
+	
+	
+    /**@author Lyes
+     * Removes all Brokers from the list
+     * @return <tt>true</tt> 
+     */
+	boolean removeAllBrokers() {
+		
+		courtiers.clear();
+		return true;
+		
+	}
+	
+    /**@author Lyes
+     * Add the specified Company  to the list (If it already is in the list).
+     * @param e : the company to be added
+     * @exception IllegalArgumentException if the Company does exist
+     * @return <tt>true</tt> 
+     */
+	boolean addCompany(Entreprise e) {
+		
+		if ( entreprises.contains(e) ) throw new IllegalArgumentException("The Broker is already in the list.");
+
+		entreprises.add(e);
+		return true;
+	}
+	
+	
+    /**@author Lyes
+     * Appends the collection of Companies to the end of the list, using addCompany(E).
+     * @param L : the collection of Companies to be appended to this list
+     * @return <tt>true</tt> 
+     */
+	boolean addAllCompanies(List<Entreprise> L) {
+		
+		for( Entreprise e : L ) {
+			
+			addCompany(e);
+		}
+		
+		return true;
+		
+	}
+	
+
+	
+    /**@author Lyes
+     * Removes the specified Company from the list (If it already is in the list).
+     * @param e : the company to be removed
+     * @exception IllegalArgumentException if the Company does exist
+     * @return <tt>true</tt> 
+     */
+	boolean removeCompany(Entreprise e) {
+		
+		if ( ! entreprises.contains(e) ) throw new IllegalArgumentException("The company does not exist.");
+	
+		entreprises.remove(e);
+		
+		return true;
+	}
+	
+	
+    /**@author Lyes
+     * Removes all Companies from the list
+     * @return <tt>true</tt> 
+     */
+	boolean removeAllCompanies() {
+		
+		entreprises.clear();
+		return true;
+		
+	}
+	
+	
+	
+}
