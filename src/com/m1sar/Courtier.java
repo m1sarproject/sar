0a6223d112fc33f865ddcb09e9de07ebe090d9ec
Nick3523 <lyes.meghara@gmail.com>
2017-12-28 00:06:04 +0100

    Initialisation de quelques variables dans la classe Courtier

--- src/com/m1sar/Courtier.java
+++ src/com/m1sar/Courtier.java
@@ -3,17 +3,18 @@ package com.m1sar;
  * 
  */
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Courtier extends Thread{
 	
 private String name;
-private ArrayList<Client>customers;
+private ArrayList<Client> customers= new ArrayList<Client>();
 /**
  * nbCustomer number of customers which are connected to this Broker
  */
 private int nbCustomer;
-private double tauxCommission;
-private double accountBalance;
+private double tauxCommission=ThreadLocalRandom.current().nextDouble(0.05, 0.1); //un taux aléatoire entre 5% et 10%
+private double accountBalance=0.;
 
 /**
  * Send to all his cutomers the updated price of the auction 
@@ -21,5 +22,13 @@ private double accountBalance;
 public void sendAllPrices() {
 	
 }
+
+public double getTauxCommission() {
+	return tauxCommission;
+}
+
+
+
+
 
 }
