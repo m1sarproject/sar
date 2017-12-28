afc7bf61ff661160e1440deefba1f66e5ca32e6d
biovitalinka94 <32669598+biovitalinka94@users.noreply.github.com>
2017-12-28 22:37:38 +0100

    Update OrdreVente.java

--- src/com/m1sar/OrdreVente.java
+++ src/com/m1sar/OrdreVente.java
@@ -3,7 +3,7 @@ package com.m1sar;
 public class OrdreVente extends Ordre{
 
 
-	public OrdreVente(Entreprise entreprise, Client client, double prix_Propose_par_Client){
+	public OrdreVente(String entreprise, Client client, double prix_Propose_par_Client){
 		
 		super(entreprise,client, prix_Propose_par_Client);
 	}
