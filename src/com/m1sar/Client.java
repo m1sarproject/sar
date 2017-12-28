20523e10106a1c7cabe8bc940b5a7f150b91fe42
biovitalinka94 <32669598+biovitalinka94@users.noreply.github.com>
2017-12-28 22:30:30 +0100

    Update Client.java

--- src/com/m1sar/Client.java
+++ src/com/m1sar/Client.java
@@ -12,7 +12,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-/**
+
+/**@author Vitalina
 La classe Client qui peut acheter ou vendre des actions
 */
 public class Client {
@@ -31,26 +32,27 @@ public class Client {
 	private Map<String,Integer> portefeuille;
 	private double solde;
 	private Courtier courtier;
-	private double depensesEventuelles;
 	
 	
-	public Client(String nameClient, double solde, Courtier courtier) {
+	public Client(String nameClient, double solde) {
 		
-		this.nameClient = nameClient;
+		
 		this.solde = solde;
-		this.courtier = courtier;
 		portefeuille=new HashMap<>();
 		ordres =new ArrayList<>();
 		idClient=cpt++;
 		
 	}
 	
+	 /**@author Vitalina
+     * 
+     * 
+     */
 	public void connexion(){
 		
 			try {
-				
 				sc= new Socket(hote,port);
-		
+			
 			in =new BufferedReader(new InputStreamReader(sc.getInputStream()));
 			out=new PrintWriter(sc.getOutputStream(),true);
 			out.println("Client "+nameClient+" veut se connecter");
@@ -62,59 +64,98 @@ public class Client {
 			}
 	}
 	
-	public void acheter (double prix, int quantite, String entreprise){
-		
-		if (! achatLegal(prix*quantite)) return;
-		
-		depensesEventuelles+= prix * quantite;
-		boolean existe=false;
-		for (Entry<String, Integer> e : portefeuille.entrySet()){
-			if(e.getKey().equals(entreprise))existe=true;
-		}
-		if(existe){
-			int i=portefeuille.get(entreprise);
-			portefeuille.replace(entreprise, quantite+i);
-		}
-		else{
-			portefeuille.put(entreprise, quantite);
-		}
-		solde-=prix;
-	}
 	
-	public void vendre (double prix, int quantite, String entreprise){
+	/**@author Vitalina
+     * 
+     * 
+     */
+	public void acheter (double prix, int quantite, Entreprise entreprise){
 		
-		if ( ! venteLegal(entreprise,quantite) ) return;
-		 
-		solde+=prix;
-		for (Entry<String, Integer> e : portefeuille.entrySet()){
-			if(e.getKey().equals(entreprise)){
-				e.setValue(e.getValue()-quantite);
-			}
-		}
+		solde-=prix+(prix*courtier.getTauxCommission());
+		Ordre r =new OrdreAchat(entreprise, this, 11);
+		ordres.add(r);
+		
 	}
 	
 	
+	/**@author Vitalina
+     * 
+     * 
+     */
+	public void vendre (double prix, int quantite, Entreprise entreprise){
+		solde+=prix;// enlever les commissions?
+		Ordre r =new OrdreVente(entreprise, this, 11);
+		ordres.add(r);
+	}
 	
+	
+	/**@author Vitalina
+     * 
+     * 
+     */
 	public boolean venteLegal(String entreprise,int quantite){
-				
-		return portefeuille.get(entreprise) < quantite;
+		if(!portefeuille.containsKey(entreprise))return false;
+		return portefeuille.get(entreprise)<quantite;
 	}
 	
-	
+	/**@author Vitalina
+     * 
+     * 
+     */
 	public boolean achatLegal(double prix){
-		double prixReel = prix + Courtier.tauxCommission * prix ;
-		return solde - depensesEventuelles  > prixReel;
+		
+		return solde> courtier.getTauxCommission();
 	}
 	
-	
+	/**@author Vitalina
+     * 
+     * 
+     */
 	public void deconnexion(){
 		//a completer 
 	}
+	/**@author Vitalina
+     * 
+     * 
+     */
+	public void majPortefeuilleAchat(String entreprise, int quantite){
+		if(true){//si ok pour aquis de reception
+			if(portefeuille.containsKey(entreprise)){
+				int i=portefeuille.get(entreprise);
+				portefeuille.replace(entreprise, quantite+i);
+			}
+			else{
+				portefeuille.put(entreprise, quantite);
+			}
+		}
+	}
+		
 	
-	public void majPortefeuille(){
+	
+	
+	/**@author Vitalina
+     * 
+     * 
+     */
+	public void majPortefeuilleVente(String entreprise, int quantite){
+		if(true){
+			if(!portefeuille.containsKey(entreprise))return;
+			int i=portefeuille.get(entreprise);
+			if(i==quantite)portefeuille.remove(entreprise);
+			else{
+			portefeuille.replace(entreprise, i-quantite);
+			}
+		}
 		
 	}
 	
+	public void setCourtier(Courtier c){
+		courtier=c;
+	}
+	
+	public Courtier getCourtier(){
+		return courtier;
+	}
 	
 	
 	
